a545abd52ca551f6e4777d0cee08a3afae80ac3b
maksontakson <maksontukson@gmail.com>
2023-03-13 18:43:09 +0200

    Improved code according to comments

--- ActionStorage-master/src/test/java/app/just/actionstorage/service/UserServiceUnitTest.java
+++ ActionStorage-master/src/test/java/app/just/actionstorage/service/UserServiceUnitTest.java
@@ -3,18 +3,16 @@ package app.just.actionstorage.service;
 import static app.just.actionstorage.common.TestConstants.Model.USER_NAME1;
 import static app.just.actionstorage.common.TestConstants.Model.UserEntityAttributes.DESCRIPTION;
 import static app.just.actionstorage.common.TestConstants.Model.UserEntityAttributes.EMAIL;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.only;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 import app.just.actionstorage.entity.UserEntity;
-import app.just.actionstorage.mapper.UserEntityMapper;
+import app.just.actionstorage.mapper.UserEntityMapperImpl;
 import app.just.actionstorage.repository.UserRepository;
 import app.just.actionstorage.serivce.UserService;
 import app.just.common.dto.UserDto;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -33,25 +31,20 @@ public class UserServiceUnitTest {
   @Mock
   private UserRepository userRepository;
   @Mock
-  private UserEntityMapper userEntityMapper;
+  private UserEntityMapperImpl userEntityMapper;
 
   @Test
-  void shouldFindAllUsersThenMapThemToDtoAndReturnListOfDto() {
-    //when
+  void whenFindAll_ThenReturnListOfMappedDto() {
     when(userRepository.findAll()).thenReturn(List.of(USER_ENTITY));
-    //execute
+    when(userEntityMapper.toDto(USER_ENTITY)).thenCallRealMethod();
     List<UserDto> all = userService.findAll();
-    //then
-    verify(userRepository, only()).findAll();
-    verify(userEntityMapper, atLeastOnce()).toDto(USER_ENTITY);
-    Assertions.assertEquals(1, all.size());
+
+    assertThat(1).isEqualTo(all.size());
   }
 
   @Test
-  void shouldThrowNullPointerExceptionWhenDbReturnNull() {
-    //when
+  void whenFindAllReturnNull_ThenNullPointerExceptionIsThrown() {
     when(userRepository.findAll()).thenReturn(null);
-    //thenThrows
-    Assertions.assertThrows(NullPointerException.class, () -> userService.findAll());
+    assertThatThrownBy(() -> userService.findAll()).isInstanceOf(NullPointerException.class);
   }
 }

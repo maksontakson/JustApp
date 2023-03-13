6deea9efd7b3971771e124b4b1a4c6bbf57318fc
maksontakson <maksontukson@gmail.com>
2023-03-13 19:51:30 +0200

    Added DB RIDER comparison

--- ActionStorage-master/src/test/java/app/just/actionstorage/repository/UserRepositoryIntegrationTest.java
+++ ActionStorage-master/src/test/java/app/just/actionstorage/repository/UserRepositoryIntegrationTest.java
@@ -1,6 +1,8 @@
 package app.just.actionstorage.repository;
 
 import app.just.actionstorage.entity.UserEntity;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.spring.api.DBRider;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@DBRider
 public class UserRepositoryIntegrationTest extends AbstractTest {
   @Autowired
   private UserRepository userRepository;
 
   @Test
+  @ExpectedDataSet("users.yml")
   public void whenFindAll_thenReturnUserEntityList() {
     List<UserEntity> all = userRepository.findAll();
     Assertions.assertEquals(3, all.size());

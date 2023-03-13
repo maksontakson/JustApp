1d99ef373c82bb9748f2fee69bb55bd28d60a51e
maksontakson <maksontukson@gmail.com>
2023-03-13 21:18:04 +0200

    Added one test and DB Rider into UserControllerTest

--- ActionStorage-master/src/test/java/app/just/actionstorage/controller/UserControllerTest.java
+++ ActionStorage-master/src/test/java/app/just/actionstorage/controller/UserControllerTest.java
@@ -8,6 +8,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import app.just.actionstorage.common.TestConstants;
 import app.just.actionstorage.repository.AbstractTest;
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,14 +19,25 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DBRider
 public class UserControllerTest extends AbstractTest {
   @Autowired
   private MockMvc mockMvc;
 
   @Test
+  @DataSet(value = "addusers.yml")
+  @DBUnit(caseSensitiveTableNames = true)
   public void givenEmployeesInDb_whenGetEmployees_ThenStatus200() throws Exception {
     this.mockMvc.perform(get(TestConstants.Path.USER_CONTROLLER_POST)).andDo(print())
         .andExpect(status().isOk()).andExpect(content()
             .string(containsString("test_user_first")));
+  }
+
+  @Test
+  @DataSet(value = "nousers.yml")
+  @DBUnit(caseSensitiveTableNames = true)
+  public void givenEmptyEmpTable_whenGetEmployees_ThenStatus200() throws Exception {
+    this.mockMvc.perform(get(TestConstants.Path.USER_CONTROLLER_POST)).andDo(print())
+        .andExpect(status().isOk()).andExpect(content().string("[]"));
   }
 }
